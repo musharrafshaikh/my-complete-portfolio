5d51d8dfb4f1415080743f6f417d35e4ed82196d
musharrafshaikh <musharrafshaikh1705@gmail.com>
2021-09-04 21:09:25 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Side Navigation Menu Js
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = function() {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflowX = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflowX = "auto";
+    scrollBtn.style.pointerEvents = "auto";
+  }
+  // Side Navigation Bar Close While We click On Navigation Links
+
+  let navLinks = document.querySelectorAll(".menu li a");
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click" , function() {
+      navBar.classList.remove("active");
+      menuBtn.style.opacity = "1";
+      menuBtn.style.pointerEvents = "auto";
+    });
+  }
